d97c5d23e32dc9a13707dfad6c8061da09f9ba77
Jürgen Hahn <eth3real89@gmail.com>
2019-03-18 16:50:32 +0100

    wrote first draft of link.md (needs proof-reading and is still very work-in-progress) as well as further work on silang

---
+++ SI-Linking-Prototype/silang_link_representation_compiler/silang.py
@@ -0,0 +1,6 @@
+# needs EBNF
+# needs lexical analysis (lexer)
+# needs parsing and syntax checking (parser)
+# needs code generation (code generator)
+    # this however is not typical code generation as the result is loaded into a SI-context or the results builds a fully usable SI-context
+
