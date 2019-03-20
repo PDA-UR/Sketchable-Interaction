96f865d0ae776888dbaf34bd11445db806cc8fe7
Jürgen Hahn <eth3real89@gmail.com>
2019-03-20 19:40:27 +0100

    first collision prototype implementation

---
+++ SI-Collision-Prototype/main.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+from Canvas import Canvas
+
+import sys
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = QMainWindow()
+
+    canvas = Canvas(1920, 1080)
+
+    sys.exit(app.exec_())
