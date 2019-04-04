4d53f9bc437e4484bc4f6657321225293c6c7732
Jürgen Hahn <eth3real89@gmail.com>
2019-04-04 09:23:11 +0200

    added own documentation folder for sub-concepts of SI; i did in this branch because i forgot to change branches

--- SI-Linking-Prototype/silang_link_representation_compiler/silang.py
+++
@@ -1,6 +0,0 @@
-# needs EBNF
-# needs lexical analysis (lexer)
-# needs parsing and syntax checking (parser)
-# needs code generation (code generator)
-    # this however is not typical code generation as the result is loaded into a SI-context or the results builds a fully usable SI-context
-
