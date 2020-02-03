b6cfd8e4ef73defeb6e529dff2d48ac9eebd85a4
Jürgen Hahn <eth3real89@gmail.com>
2020-02-03 17:13:33 +0100

    refactored RegionRepresentation; refactoring

--- SI/src/siren/region/RegionRepresentation.hpp
+++ SI/src/siren/region/RegionRepresentation.hpp
@@ -10,51 +10,9 @@
 
 struct RegionRepresentation
 {
-    RegionRepresentation(const std::vector<glm::vec3>& c, const glm::mat3x3& t, const glm::vec4& col, const std::string& _name, const std::string& tex_path, QWidget* parent):
-            r(col.r),
-            g(col.g),
-            b(col.b),
-            a(col.a),
-            color(QColor(col.r, col.g, col.b, col.a)),
-            source_contour(c),
-            texture_path(tex_path),
-            name(_name),
-            view(std::make_unique<QQuickWidget>())
-    {
-        fill.moveTo(c[0].x, c[0].y);
+    RegionRepresentation(const std::vector<glm::vec3>& c, const glm::mat3x3& t, const glm::vec4& col, const std::string& _name, const std::string& tex_path, QWidget* parent);
 
-        for(auto& p: c)
-        {
-            fill.lineTo(p.x, p.y);
-            poly << QPoint(p.x, p.y);
-        }
-
-        poly << QPoint(c[0].x, c[0].y);
-
-        view->setParent(parent);
-//        view->setSource(QUrl::fromLocalFile("plugins/standard_environment_library/cursor/qml/test.qml"));
-//        view->show();
-    }
-
-    void update(const glm::mat3x3& transform)
-    {
-        fill = QPainterPath();
-        poly = QPolygonF();
-
-        for(glm::vec3& p: source_contour)
-        {
-            glm::vec3 p_ = glm::vec3(p.x, p.y, 1) * transform;
-            p_.x /= p_.z;
-            p_.y /= p_.z;
-
-            poly << QPoint(p_.x, p_.y);
-        }
-
-        fill.moveTo(poly[0].x(), poly[0].y());
-
-        for(int i = 1; i < poly.size(); i++)
-            fill.lineTo(poly[i]);
-    }
+    void update(const glm::mat3x3& transform);
 
     int r;
     int g;
@@ -67,7 +25,6 @@ struct RegionRepresentation
     QPainterPath fill;
     std::vector<glm::vec3> source_contour;
     std::unique_ptr<QQuickWidget> view;
-
 };
 
 #endif //SITEST_REGIONREPRESENTATION_HPP
